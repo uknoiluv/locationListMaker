f1898cd9439c32c039809956610c3b07abc2da94
Adrian Kim <uknoiluv@gmail.com>
2014-10-25 11:23:10 -0700

    10/25_searchBox_added

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ angular.module('map', ['google-maps'.ns()])
     GoogleMapApi.configure({
         // key: 'your api key',
         v: '3.17',
-        libraries: 'weather,geometry,visualization'
+        libraries: 'weather,geometry,visualization,places',
+        sensor: 'false'
     });
   }]).controller('mapController',['$scope', 'GoogleMapApi'.ns(), function($scope, GoogleMapApi){
     // do stuff with scope
+    var x = {
+      places_changed: function(data){
+        console.log(data);
+        console.log(data.getPlaces());
+      }
+    };
     $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14 };
     $scope.options = {scrollwheel: false};
+    $scope.searchbox = {template:'searchbox.tpl.html', position:'top-left', events: x};
 
       GoogleMapApi.then(function(maps){
+        maps.event
 
       });
     }
