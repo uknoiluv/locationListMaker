a8c0a847f651b91a6aef37bc8182acc0a727558b
Adrian Kim <uknoiluv@gmail.com>
2014-10-25 14:16:24 -0700

    10/25_marker_added

--- app.js
+++ app.js
@@ -9,18 +9,27 @@ angular.module('map', ['google-maps'.ns()])
     });
   }]).controller('mapController',['$scope', 'GoogleMapApi'.ns(), function($scope, GoogleMapApi){
     // do stuff with scope
-    var x = {
+    var getLoc = {
       places_changed: function(data){
-        console.log(data);
-        console.log(data.getPlaces());
+        var places = data.getPlaces();
+        var latitude = places[0].geometry.location.k;
+        var longitude = places[0].geometry.location.B;
+        $scope.map.center = {latitude: latitude, longitude: longitude};
+        $scope.marker.coords = {latitude: latitude, longitude: longitude};
       }
     };
     $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14 };
     $scope.options = {scrollwheel: false};
-    $scope.searchbox = {template:'searchbox.tpl.html', position:'top-left', events: x};
+    $scope.searchbox = {template:'searchbox.tpl.html', position:'top-left', events: getLoc};
+    $scope.marker = {
+      id: 0,
+      coords: $scope.map.center,
+      options: {
+        draggable: false
+      }
+    };
 
       GoogleMapApi.then(function(maps){
-        maps.event
 
       });
     }
