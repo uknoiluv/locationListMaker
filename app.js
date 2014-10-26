3b83e6dd8ad75d5edb21f2c2048e322fce95fbe2
Adrian Kim <uknoiluv@gmail.com>
2014-10-26 11:19:49 -0700

    10/26_added_service_for_storing_marker

--- app.js
+++ app.js
@@ -8,7 +8,28 @@ var app = angular.module('map', ['google-maps'.ns(), 'ui.router']);
     $stateProvider
       .state('findPlace', {
         url: '/findPlace',
-        template: '<div>findPlace</div>'
+        template: '<div>findPlace</div>' +
+        '<div id="map_canvas">' +
+
+    '<script type="text/ng-template" id="searchbox.tpl.html">' +
+      '<input id="pac-input" class="controls" type="text" placeholder="Search Box"></input>' +
+    '</script>' +
+
+    '<script type="text/ng-template" id="control.tpl.html">' +
+        '<button class="btn" ng-click="controlClick()">{{controlText}}</button>' +
+    '</script>' +
+
+        '<ui-gmap-google-map center="map.center" zoom="map.zoom" draggable="true" options="options">' +
+      '<ui-gmap-search-box template="searchbox.template" position="searchbox.position" events="searchbox.events">' +
+      '</ui-gmap-search-box>' +
+      
+      '<ui-gmap-map-control template="control.tpl.html" position="top-left" controller="customController" index="-1"></ui-gmap-map-control>' +
+      
+      '<ui-gmap-marker coords="marker.coords" options="marker.options" idkey="marker.id">' +
+      '</ui-gmap-marker>' +
+    '</ui-gmap-google-map>' +
+    '</div>',
+        controller: 'mapController'
         // templateUrl: '/partials/findPlace.html'
       })
       .state('savedList', {
@@ -29,13 +50,12 @@ var app = angular.module('map', ['google-maps'.ns(), 'ui.router']);
         sensor: 'false'
     });
 
-  }]).controller('mapController',['$scope', 'GoogleMapApi'.ns(), function($scope, GoogleMapApi){
+  }]).controller('mapController', ['$scope', 'GoogleMapApi'.ns(), 'Data', function($scope, GoogleMapApi, Data){
     // do stuff with scope
-    var searched = false;
     var getLoc = {
       places_changed: function(data){
-        console.log('searched1', searched);
-        searched = true;
+        Data.searched = Data.searched || true;
+        console.log('Data1', Data);
         var places = data.getPlaces();
         console.log('places1', places[0]);
         var latitude = places[0].geometry.location.k;
@@ -45,8 +65,9 @@ var app = angular.module('map', ['google-maps'.ns(), 'ui.router']);
         $scope.marker.info = places[0];
         places[0].date = new Date();
         console.log('places2', places[0]);
-        $scope.searchHistory.push(places[0]);
-        console.log('searched2', searched);
+        Data.searchHistory.push(places[0]);
+        Data.marker = $scope.marker.info;
+        console.log('$scope1', $scope);
       }
     };
     $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14 };
@@ -59,19 +80,32 @@ var app = angular.module('map', ['google-maps'.ns(), 'ui.router']);
         draggable: false
       }
     };
-    $scope.searchHistory = [];
-    $scope.savedList = [];
-    $scope.controlText = 'save the marker to the list';
-    $scope.controlClick = function(){
-      if(searched && !$scope.savedList[$scope.marker.info.id]){
-        $scope.savedList[$scope.marker.info.id] = $scope.marker.info;
-      }else{
-        // show "already in the list"
-      }
-    };
 
       GoogleMapApi.then(function(maps){
 
       });
     }
   ]);
+
+  app.controller('customController', ['$scope', 'GoogleMapApi'.ns(), 'Data', function($scope, GoogleMapApi, Data){
+    $scope.controlText = 'save the marker to the list';
+    $scope.controlClick = function(){
+      console.log('Data2', Data);
+      console.log('_.some', _.some(Data.savedList, function(item){
+        return item.id === Data.marker.id
+      }));
+      if(Data.searched && !_.some(Data.savedList, function(item){
+        return item.id === Data.marker.id
+      })){
+        Data.savedList.push(Data.marker);
+      }else{
+        console.log('already in the list')
+        // show "already in the list"
+      }
+      console.log('Data.savedList', Data.savedList);
+    };
+  }]);
+
+  app.factory('Data', function(){
+    return {searched: false, savedList: [], searchHistory: [], marker:{}};
+  });
