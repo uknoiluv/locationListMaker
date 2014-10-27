df858939631affc97c8e24820b7debfbd7cfcf33
Adrian Kim <uknoiluv@gmail.com>
2014-10-27 16:22:50 -0700

    10/27_refactoring

---
+++ directives.js
@@ -0,0 +1,16 @@
+
+app.directive('savedInlist', function(){
+  return {
+    link: function(scope, element, attr){
+      scope.$watch('controlText', function(newValue, oldValue){
+        if(newValue === 'saved'){
+          element.css('background-color', '#000066');
+        }else if(newValue === 'already in the list'){
+          element.css('background-color', '#FF3399');
+        }else{
+          element.css('background-color', '#0099CC');
+        }
+      }, true);
+    }
+  };
+});
---
+++ factories.js
@@ -0,0 +1,9 @@
+
+app.factory('Data', function(){
+  return {defaultLoc:{latitude: 34.440009, longitude: -119.738670} ,searched: false, savedList: [], searchHistory: [], marker:{}};
+});
+
+
+// app.factory('GoogleMapApiService', ['GoogleMapApiService'.ns(), function(GoogleMapApi){
+//   return GoogleMapApi;
+// }]);
