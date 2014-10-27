8ce862dd5c2e5578a87d6494e764d3790f7540b3
Adrian Kim <uknoiluv@gmail.com>
2014-10-27 09:46:09 -0700

    10/27_test_working

--- mapSpec.js
+++ mapSpec.js
@@ -1,2 +1,31 @@
 
 // test
+describe('Location List Maker factory', function(){
+  
+  // console.log('app', app);
+
+  beforeEach(module('map'));
+
+  var data;
+
+  beforeEach(inject(function(_Data_){
+    data = _Data_;
+  }));
+
+  describe('when invoked', function(){
+    
+    // beforeEach(function(){
+    //   var dataInside = data();
+    // });
+   
+    it('should be values for properties', function(){
+      expect(data.searched).to.equal(false);
+    });
+
+  });
+
+  // it('describe module' ,function(){
+
+  // });
+
+});
